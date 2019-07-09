bd1cd6c987c01eaa58da12ad5540d77607166fc1
chernikovmax <chernik.max@gmail.com>
2019-07-09 14:21:57 +0300

    [ADD] Dynamically changing of the description in photos when loading data.

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,22 +3,23 @@ import PropTypes from 'prop-types';
 
 export class Page extends React.Component {
     onBtnClick = e => {
-        const year = +e.currentTarget.innerText;        
-        this.props.setYear(year);
+        const year = parseInt(e.currentTarget.innerText, 10);        
+        this.props.getPhotos(year);
     }
     render() {
-        const {year, photos} = this.props;
+        const {year, photos, isFetching} = this.props;
         return (
-            <div>
-                <div>
-                    <button onClick={this.onBtnClick}>2019</button>
-                    <button onClick={this.onBtnClick}>2018</button>
-                    <button onClick={this.onBtnClick}>2017</button>
-                    <button onClick={this.onBtnClick}>2016</button>
-                    <button onClick={this.onBtnClick}>2015</button>
-                    <button onClick={this.onBtnClick}>2014</button>
+            <div className="page">
+                <div className="btn-wrapper">
+                    <button className="btn" onClick={this.onBtnClick}>2019</button>
+                    <button className="btn" onClick={this.onBtnClick}>2017</button>
+                    <button className="btn" onClick={this.onBtnClick}>2018</button>
+                    <button className="btn" onClick={this.onBtnClick}>2016</button>
+                    <button className="btn" onClick={this.onBtnClick}>2015</button>
+                    <button className="btn" onClick={this.onBtnClick}>2014</button>
                 </div>
-                <p>You have {photos.length} photos in {year} year.</p>
+                <h2>{year} year.</h2>
+                {isFetching ? <p>Downloading...</p> : <p>You have {photos.length} photos.</p>}
             </div>
         );
     }
@@ -27,5 +28,6 @@ export class Page extends React.Component {
 Page.propTypes = {
     year: PropTypes.number.isRequired,
     photos: PropTypes.array.isRequired,
-    setYear: PropTypes.func.isRequired,
+    getPhotos: PropTypes.func.isRequired,
+    isFetching: PropTypes.bool.isRequired,
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,21 +2,23 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {User} from '../components/User';
 import {Page} from '../components/Page';
-import {setYear} from '../actions/PageActions';
+import {getPhotos} from '../actions/PageActions';
 
 import './App.css';
 
 
 class App extends Component {
   render() {
-    const {user, page, setYearAction} = this.props;
+    const {user, page, getPhotosAction} = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">My top photos</h1>
-        </header>
+      <div className="app">
+        <Page 
+          photos={page.photos} 
+          year={page.year} 
+          isFetching={page.isFetching}
+          getPhotos={getPhotosAction}
+        />
         <User name={user.name} />
-        <Page photos={page.photos} year={page.year} setYear={setYearAction}/>
       </div>
     );
   }
@@ -30,7 +32,7 @@ const mapStateToProps = store => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    setYearAction: year => dispatch(setYear(year)),
+    getPhotosAction: year => dispatch(getPhotos(year)),
 });
 
 export default connect(
